1431a8184fa2e5ba24a1890438b87e599a982cff
daler <dalerr@niddk.nih.gov>
2014-11-05 13:08:41 -0500

    processor clarification

--- processor.py
+++ processor.py
@@ -1,16 +1,21 @@
+#!/usr/bin/env python
 import sys
 import textwrap
 
 for line in open(sys.argv[1]):
+
     if line.startswith('# In['):
         continue
+
     if line.startswith('#'):
         for i in textwrap.wrap(line.lstrip('# ')):
             print '# ' + i
+        continue
     if line.startswith('get_ipython'):
         print '# ' + line,
-    else:
-        print line,
+        continue
+
+    print line,
 
 print "plt.show()"
 
